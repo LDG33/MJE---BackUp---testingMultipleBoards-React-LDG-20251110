6421c29db0a6ea76242029f6567462bc04dad648
LDG33 <118735787+LDG33@users.noreply.github.com>
2025-11-14 17:19:06 +0000

    added about section and trying margin on main container if it will add margin on mobile views

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,19 @@ const About = () => {
         <div className="stylingAbout">
             <h1 className="landingHeader2">About</h1>
             <p className="MJEdescription2">
-              MJE stands for MahJong Europa. It is a website that promotes alternative approach to learn 
-                languages through gamification. MJE is a test approach created by Lukasz Galik - Edinburgh 
-                Napier University student as part of the course. MahJong Europa will allow you to learn 
-                most popular foreign languages and get enough vocabulary to achieve A2 level of language 
-                comprehantion. The strength of MJE is that you can learn three languages at once. Thank you 
-                for visiting this site and have fun.
+                As presented at the homepage, the MahJong Europa (MJE) was developed by Lukasz Galik as part
+                of Honours Project at Edinburgh Napier University. MJE is a language learning app available as a website.
+            </p>
+            <p className="MJEdescription2">
+                MJE was developed in JavaScript together with React.js library/framework.
+                It is a front end only at the moment. It uses Vite to set up web app environment.
+                It uses JSX to incorporate an HTML code into the JavaScript - which is a common 
+                practice in React.js. 
+            </p>
+            <p className="MJEdescription2">
+              MJE is being stored and backed up using GitHub repository. GitHub is connected
+              with Netlify and MJE is hosted at the following URL address: https://mahjongeuropa2.netlify.app/
+              Netlify is a fee hosting provider for front-end websites.
             </p>
         </div>
         <div className="stylingQuickAccess">
